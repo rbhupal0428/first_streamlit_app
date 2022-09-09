0e9c8ea25d88b2cd54b8d29ebec1f2f280885d8f
rbhupal0428 <113211897+rbhupal0428@users.noreply.github.com>
2022-09-09 15:55:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Medhu Vada with sambar')
 streamlit.text('Scrabled eggs and toast with some hashbrowns')  
 streamlit.text('Avocado Toast')
