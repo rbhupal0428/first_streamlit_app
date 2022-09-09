e5b1c5a6f2af6a117acb49e14195536cb5c76523
rbhupal0428 <113211897+rbhupal0428@users.noreply.github.com>
2022-09-09 15:51:25 -0500

    Update streamlit_app.py
    added some items to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Pannet Tikka Masala)
+streamlit.text('Medhu Vada with sambar')
 streamlit.text('Scrabled eggs and toast with some hashbrowns')               
