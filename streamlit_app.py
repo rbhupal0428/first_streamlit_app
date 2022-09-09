49e5d81827d96f6a43c7384b3fa3e115dd636c42
rbhupal0428 <113211897+rbhupal0428@users.noreply.github.com>
2022-09-09 15:50:36 -0500

    Update streamlit_app.py
    added some items to breakfast menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Pannet Tikka Masala)
+streamlit.text('Scrabled eggs and toast with some hashbrowns')               
